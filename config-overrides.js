33bafa63216ff1902dfcabd38dd271fe16d0d443
左国梁 <3059349417@qq.com>
2021-02-10 21:33:43 +0800

    first commit

---
+++ config-overrides.js
@@ -0,0 +1,26 @@
+const {
+    override,
+    addLessLoader, // less配置函数
+    fixBabelImports, // 按需加载配置函数
+    addBabelPlugins // babel插件配置函数
+} = require('customize-cra')
+
+module.exports = override(
+    ...addBabelPlugins( // 支持装饰器
+        [
+            '@babel/plugin-proposal-decorators',
+            { legacy: true}
+        ]
+    ),
+    fixBabelImports('import', { // antd 按需加载
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true  //自动打包相关的样式 默认为 style:'css'
+    }),
+    addLessLoader(
+        {
+            javascriptEnabled: true,
+            modifyVars: { '@primary-color': 'red' }
+        }
+    ),
+)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Button } from 'antd';
+import styles from './demo.module.less'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.span}>
+      <Button type="primary">点击</Button>
     </div>
   );
 }
